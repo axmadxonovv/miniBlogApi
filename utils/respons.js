9eaf8dd5194822745b83c9ab917f6ff1626f5bcd
Avazbek <apple@MacBook-Pro-apple.local>
2025-03-25 15:15:55 +0500

    respons qaytaradigan yozdim

---
+++ utils/respons.js
@@ -0,0 +1,15 @@
+let getRespons = (res, status, data) => {
+  if (status >= 200 && status <= 205) {
+    res.status(status).json({
+      status: "Succes",
+      data,
+    });
+  } else {
+    res.status(status).json({
+      status: "Failed",
+      message: data,
+    });
+  }
+};
+
+module.exports = getRespons;
